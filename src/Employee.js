6282319c1427d07d53fea6536aa9890a18046b6b
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-05-30 21:36:58 +0530

    1 file modified

--- src/Employee.js
+++ src/Employee.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-import Axios from 'axios';
+//import Axios from 'axios';
 export default function Employee() {
   const [valuesName, setValuesNmae] = useState('');
+  const optionData = {
+    method: 'Get',
+    headers: { 'Content-Type': 'application/json' },
+  };
   useEffect(() => {
-    Axios.get;
+    fetch('https://dummyjson.com/products', optionData)
+      .then((response) => {
+        setValuesNmae(response.data);
+      })
+      .catch((error) => {
+        console.log('We can not fetch api' + error);
+      });
   }, []);
 
   return (
